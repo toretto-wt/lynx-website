2e0f5eb6a8d455a1cb484d8f0508d5ba3fa2c719
Leron Liu <15890184+loongliu@users.noreply.github.com>
2025-09-19 12:26:40 +0800

    add base next (#386)

--- rspress.config.ts
+++ rspress.config.ts
@@ -93,6 +93,7 @@ export default defineConfig({
       'https://lf-lynx.tiktok-cdns.com/obj/lynx-artifacts-oss-sg/lynx-website/assets/lynx-dark-logo.svg',
     dark: 'https://lf-lynx.tiktok-cdns.com/obj/lynx-artifacts-oss-sg/lynx-website/assets/lynx-light-logo.svg',
   },
+  base: '/next',
   themeConfig: {
     locales: [
       {
